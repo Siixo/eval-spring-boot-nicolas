4b135e24e08a44f6d5e008750d44fd7caccdf925
Nicolas Knittel <ohsiixo@gmail.com>
2026-01-05 13:56:15 +0100

    Premier commit

---
+++ src/test/java/com/siixo/evalspring/EvalspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siixo.evalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvalspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
